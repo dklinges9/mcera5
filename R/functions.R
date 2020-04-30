dd9b92525aed73bca1578f501baa4f391586f875
everydayduffy <james.philip.duffy@gmail.com>
2020-04-30 11:04:20 +0100

    Initial commit.

---
+++ R/functions.R
@@ -0,0 +1,367 @@
+# calculate humidity from dew point temperature
+# tdew = Dewpoint temperature (deg C)
+# tc = temperature (deg C)
+# p = pressure (pascals)
+.humfromdew <- function(tdew, tc, p) {
+  pk <- p/1000
+  ea <- 0.6108 * exp(17.27 * tdew / (tdew + 237.3))
+  e0 <- 0.6108 * exp(17.27 * tc/(tc + 237.3))
+  s <- 0.622 * e0/pk
+  hr <- (ea/e0) * 100
+  hs <- (hr/100) * s
+  hs # specific humidity
+}
+
+# correct radiation for being an average over the hour rather than on the hour
+.rad_calc <- function(rad, tme, long, lat) {
+
+  bound <- function(x, mn = 0, mx = 1) {
+    x[x > mx] <- mx
+    x[x < mn] <- mn
+    x
+  }
+
+  tme1h <- as.POSIXlt(tme, tz = "UTC", origin = "1970-01-01 00:00")
+  # interpolate times to 10 mins
+  tme10min <- as.POSIXlt(seq(from = as.POSIXlt(tme1h[1]-3600),
+                             to = as.POSIXlt(tme1h[length(tme1h)]),
+                             by = "10 min"), origin = "1970-01-01 00:00",
+                         tz = "UTC")
+  csr10min <- microclima::clearskyrad(tme10min, lat = lat , long = long,
+                                      merid = 0, dst = 0)
+
+  # mean csr per hour (every 6 values)
+  csr1h <- tibble::tibble(csr10min) %>%
+    dplyr::group_by(group = gl(length(csr10min)/6, 6)) %>%
+    dplyr::summarise(csr1h = mean(csr10min)) %>%
+    .$csr1h
+
+  # watch out for NAs here - need modding?
+  od <- bound(rad / csr1h)
+  #if (is.na(od)[1]) od[1] <- mean(od, na.rm = T)
+  #if (is.na(od)[length(od)]) od[length(od)] <- mean(od, na.rm = T)
+  od[is.na(od)] <- 0
+
+  # hourly time stamps on the half hour
+  tme1h_2 <- as.POSIXlt(tme1h - 1800, tz = "UTC", origin = "1970-01-01 00:00")
+  # length to interpolate to
+  n <- length(tme1h_2) * 2
+  # work out od values for every half hour
+  od1h <- spline(tme1h_2, od, n = n)$y
+  # select just the ones on the hour
+  od1h <- od1h[seq(2,length(od1h),2)]
+  # csr on the hour
+  csr1h_2 <- microclima::clearskyrad(tme1h, lat = lat, long = long, merid = 0, dst = 0)
+  # calculate corrected rad on the hour
+  rad_out <- csr1h_2 * od1h
+  rad_out[is.na(rad_out)] <- 0
+
+  return(rad_out)
+}
+
+# calculate position of 4 nearest neighbours to a point, and xy distance to each.
+.focal_dist <- function(x, y) {
+
+  # round to nearest 0.25
+  x_r <- plyr::round_any(x, .25)
+  y_r <- plyr::round_any(y, .25)
+
+  # work out locations of the four neighbour points in the ERA5 dataset
+  if(x >= x_r) {
+    focal_x <- c(x_r, x_r, x_r + 0.25, x_r + 0.25) } else {
+      focal_x <- c(x_r - 0.25, x_r - 0.25, x_r, x_r)
+    }
+  if(y_in >= y_r) {
+    focal_y <- c(y_r, y_r + 0.25, y_r, y_r + 0.25) } else {
+      focal_y <- c(y_r - 0.25, y_r, y_r - 0.25, y_r)
+    }
+
+  # work out weighting based on dist between input & 4 surrounding points
+  x_dist <- abs(x - focal_x)
+  y_dist <- abs(y - focal_y)
+  xy_dist <- sqrt(x_dist^2 + y_dist^2)
+
+  focal <- data.frame(x = focal_x, y = focal_y, xy_dist)
+
+  return(focal)
+}
+
+# process relevant hourly climate data from an ERA5 nc to data frame
+.nc_to_df <- function(nc, x, y, start_time, end_time) {
+
+  dat <- tidync::tidync(nc) %>%
+    tidync::hyper_filter(longitude = longitude == x,
+                         latitude = latitude == y) %>%
+    tidync::hyper_tibble() %>%
+    dplyr::mutate(., obs_time = lubridate::ymd_hms("1900:01:01 00:00:00") + (time * 3600),
+                  timezone = lubridate::tz(obs_time)) %>% # convert to readable times
+    dplyr::filter(., obs_time >= start_time & obs_time < end_time + 1) %>%
+    dplyr::rename(., pressure = sp) %>%
+    dplyr::mutate(., temperature = t2m - 273.15, # kelvin to celcius
+                  humidity = .humfromdew(d2m - 273.15, temperature, pressure),
+                  windspeed = sqrt(u10^2 + v10^2),
+                  windspeed = microclima::windheight(windspeed, 10, 2),
+                  winddir = (atan2(u10, v10) * 180/pi + 180)%%360,
+                  cloudcover = tcc * 100,
+                  netlong = abs(msnlwrf) * 0.0036,
+                  downlong = msdwlwrf * 0.0036,
+                  uplong = netlong + downlong,
+                  emissivity = downlong/uplong, # converted to MJ m-2 hr-1
+                  jd = microclima::julday(lubridate::year(obs_time),
+                                          lubridate::month(obs_time),
+                                          lubridate::day(obs_time)),
+                  si = microclima::siflat(lubridate::hour(obs_time), y, x, jd)) %>%
+    dplyr::mutate(., rad_dni = fdir * 0.000001,
+                  rad_glbl = ssrd * 0.000001,
+                  rad_glbl = .rad_calc(rad_glbl, obs_time, x, y), # fix hourly rad
+                  rad_dni = .rad_calc(rad_dni, obs_time, x ,y), # fix hourly rad
+                  rad_dif = rad_glbl - rad_dni * si) %>% # converted to MJ m-2 hr-1 from J m-2 hr-1
+    dplyr::mutate(., szenith = 90 - microclima::solalt(lubridate::hour(obs_time),
+                                                       y, x, jd, merid = 0)) %>%
+    dplyr::select(.,obs_time, temperature, humidity, pressure, windspeed,
+                  winddir, emissivity, cloudcover, netlong, uplong, downlong,
+                  rad_dni, rad_dif, szenith, timezone)
+
+  return(dat)
+}
+
+# process relevant precipitation data from an ERA5 nc to data frame
+.nc_to_df_precip <- function(nc, x, y, start_time, end_time) {
+
+  dat <- tidync::tidync(nc) %>%
+    tidync::hyper_filter(longitude = longitude == x,
+                         latitude = latitude == y) %>%
+    tidync::hyper_tibble() %>%
+    dplyr::mutate(., obs_time = lubridate::ymd_hms("1900:01:01 00:00:00") + (time * 3600),
+                  timezone = lubridate::tz(obs_time)) %>% # convert to readable times
+    dplyr::filter(., obs_time >= start_time & obs_time < end_time + 1) %>%
+    dplyr::rename(., precipitation = tp) %>%
+    dplyr::select(., obs_time, precipitation)
+
+  return(dat)
+}
+
+# provide weighted neighbour calculation of all hourly variables in a time frame
+
+#' Produces spatially weighted hourly data for a single location ready for use with runauto
+#'
+#' @description `point_nc_to_df` takes an nc file or set of nc files containing
+#' hourly ERA5 climate data, and for a given set of coordinates, produces an
+#' inverse distance weighted mean of each variable, ready for use with
+#' microclima::runauto.
+#'
+#' @param nc character vector containing nc filename(s). Data within nc files
+#' must span the period defined by start_time and end_time.
+#' @param x longitude of the location for which data is required (decimal
+#' degrees, -ve west of Greenwich Meridian).
+#' @param y latitude of the location for which data is required (decimal
+#' degrees, -ve south of the equator).
+#' @param start_time a POSIXlt object indicating the first hour for which data
+#' is required.
+#' @param end_time a POSIXlt object indicating the last hour for which data
+#' is required.
+#' @param lsm_nc character vector containing the ERA5 land/sea mask nc filename.
+#' @param lsm_thresh threshold to divide land (0) and sea (1). Default = 0.05.
+#'
+#' @return a data frame containing hourly values for a suite of climate
+#' variables.
+#' @export
+#'
+#' @examples
+#'
+point_nc_to_df <- function(nc, x, y, start_time, end_time, lsm_nc = NULL,
+                           lsm_thresh = 0.05) {
+
+  if(!is.null(lsm_nc)) {
+
+    # load and correct land sea mask / apply threshold
+    lsm <- tidync::tidync(lsm_nc) %>%
+      tidync::hyper_tibble() %>%
+      dplyr::filter(., lsm > lsm_thresh) %>%
+      dplyr::select(x = longitude, y = latitude, lsm) %>%
+      dplyr::mutate(., x = dplyr::case_when(
+        x > 180 ~ (x - 360),
+        TRUE ~ as.numeric(x)))
+  }
+
+  # if the desired coords fall right on an ERA5 point - weighting calc not needed
+  if(sum((x %% .25) + (y %% .25)) != 0) {
+
+    # if using lsm
+    if(!is.null(lsm_nc)) {
+      focal <- .focal_dist(x, y) %>%
+        dplyr::mutate(., inverse_weight = 1/sum(1/(1/sum(xy_dist) * xy_dist)) * 1/(1/sum(xy_dist) * xy_dist)) %>%
+        dplyr::inner_join(., lsm, by = c("x","y"))
+
+      if(nrow(focal) == 0){
+        stop(strwrap(paste0("No land pixels to use in weighting. Provide a different location or reduce the land/sea mask threshold.", collapse = "\n")))
+      } else if(nrow(focal) < 4) {
+        warning(strwrap(paste0(4-nrow(focal), " neighbours are sea pixels and have been omitted. Optionally provide a different location or reduce the land/sea mask threshold to increase the number of neighbours.")), collapse = "\n")
+      }
+
+    } else {
+
+      focal <- .focal_dist(x, y) %>%
+        dplyr::mutate(., inverse_weight = 1/sum(1/(1/sum(xy_dist) * xy_dist)) * 1/(1/sum(xy_dist) * xy_dist))
+    }
+
+    # collector per nc file
+    nc_collect <- list()
+
+    for(i in 1:length(nc)) {
+
+      # collector per weighted neighbour
+      focal_collect <- list()
+
+      # gather and manipulate data for each focal point
+      for(j in 1:nrow(focal)) {
+
+        dat <- .nc_to_df(nc[i], focal$x[j], focal$y[j], start_time, end_time) %>%
+          dplyr::mutate(., inverse_weight = focal$inverse_weight[j])
+
+        # add to collector
+        focal_collect[[j]] <- dat
+      }
+
+      w_dat <- bind_rows(focal_collect, .id = "neighbour") %>%
+        dplyr::group_by(., obs_time)%>%
+        dplyr::summarise_at(., vars(temperature, humidity, pressure, windspeed,
+                                    winddir, emissivity, cloudcover, netlong,
+                                    uplong, downlong, rad_dni, rad_dif, szenith),
+                            weighted.mean, w = quo(inverse_weight)) %>%
+        dplyr::mutate(., timezone = "UTC")
+
+      nc_collect[[i]] <- w_dat
+    }
+
+    # skip weighting calc - use original coords
+  } else {
+
+    message("Coordinates match ERA5 grid, no weighting calculation required.")
+
+    # collector per nc file
+    nc_collect <- list()
+
+    for(i in 1:length(nc)) {
+
+      dat <- .nc_to_df(nc[i], x, y, start_time, end_time)
+      nc_collect[[i]] <- dat
+    }
+  }
+
+  out <- dplyr::bind_rows(nc_collect)
+  return(out)
+}
+
+#' Produces spatially weighted hourly data for a single location ready for use with runauto
+#'
+#' @description `point_nc_to_df_precip` takes an nc file or set of nc files
+#' containing hourly ERA5 climate data, and for a given set of coordinates,
+#' produces an inverse distance weighted mean of daily precipitation, ready for
+#' use with microclima::runauto.
+#'
+#' @param nc character vector containing nc filename(s). Data within nc files
+#' must span the period defined by start_time and end_time.
+#' @param x longitude of the location for which data is required (decimal
+#' degrees, -ve west of Greenwich Meridian).
+#' @param y latitude of the location for which data is required (decimal
+#' degrees, -ve south of the equator).
+#' @param start_time a POSIXlt object indicating the first hour for which data
+#' is required.
+#' @param end_time a POSIXlt object indicating the last hour for which data
+#' is required.
+#' @param lsm_nc character vector containing the ERA5 land/sea mask nc filename.
+#' @param lsm_thresh threshold to divide land (0) and sea (1). Default = 0.05.
+#' #'
+#' @return a numeric vector of daily precipitation.
+#' @export
+#'
+#' @examples
+#'
+point_nc_to_df_precip <- function(nc, x, y, start_time, end_time, lsm_nc = NULL,
+                                  lsm_thresh = 0.05) {
+
+  if(!is.null(lsm_nc)) {
+
+    # load and correct land sea mask / apply threshold
+    lsm <- tidync::tidync(lsm_nc) %>%
+      tidync::hyper_tibble() %>%
+      dplyr::filter(., lsm > lsm_thresh) %>%
+      dplyr::select(x = longitude, y = latitude, lsm) %>%
+      dplyr::mutate(., x = dplyr::case_when(
+        x > 180 ~ (x - 360),
+        TRUE ~ as.numeric(x)))
+  }
+
+  # if the desired coords fall right on an ERA5 point - weighting calc not needed
+  if(sum((x %% .25) + (y %% .25)) != 0) {
+
+    # if using lsm
+    if(!is.null(lsm_nc)) {
+      focal <- .focal_dist(x, y) %>%
+        dplyr::mutate(., inverse_weight = 1/sum(1/(1/sum(xy_dist) * xy_dist)) * 1/(1/sum(xy_dist) * xy_dist)) %>%
+        dplyr::inner_join(., lsm, by = c("x","y"))
+
+      if(nrow(focal) == 0){
+        stop(strwrap(paste0("No land pixels to use in weighting. Provide a different location or reduce the land/sea mask threshold.", collapse = "\n")))
+      } else if(nrow(focal) < 4) {
+        warning(strwrap(paste0(4-nrow(focal), " neighbours are sea pixels and have been omitted. Optionally provide a different location or reduce the land/sea mask threshold to increase the number of neighbours.")), collapse = "\n")
+      }
+
+    } else {
+
+      focal <- .focal_dist(x, y) %>%
+        dplyr::mutate(., inverse_weight = 1/sum(1/(1/sum(xy_dist) * xy_dist)) * 1/(1/sum(xy_dist) * xy_dist))
+    }
+
+    # collector per nc file
+    nc_collect <- list()
+
+    for(i in 1:length(nc)) {
+
+      # collector per weighted neighbour
+      focal_collect <- list()
+
+      # gather and manipulate data for each focal point
+      for(j in 1:nrow(focal)) {
+
+        dat <- .nc_to_df_precip(nc[i], focal$x[j], focal$y[j], start_time, end_time) %>%
+          dplyr::mutate(., inverse_weight = focal$inverse_weight[j])
+
+        # add to collector
+        focal_collect[[j]] <- dat
+      }
+
+      w_dat <- bind_rows(focal_collect, .id = "neighbour") %>%
+        dplyr::group_by(., obs_time) %>%
+        dplyr::summarise(., precipitation = weighted.mean(precipitation,
+                                                          w = inverse_weight))
+
+      nc_collect[[i]] <- w_dat
+    }
+
+    # skip weighting calc - use original coords
+  } else {
+
+    message("Coordinates match ERA5 grid, no weighting calculation required.")
+
+    # collector per nc file
+    nc_collect <- list()
+
+    for(i in 1:length(nc)) {
+
+      dat <- .nc_to_df_precip(nc[i], x, y, start_time, end_time)
+      nc_collect[[i]] <- dat
+    }
+  }
+
+  out <- dplyr::bind_rows(nc_collect) %>%
+    dplyr::mutate(., jd = microclima::julday(lubridate::year(obs_time),
+                                             lubridate::month(obs_time),
+                                             lubridate::day(obs_time))) %>%
+    dplyr::group_by(., jd) %>%
+    dplyr::summarise(., daily_precip = sum(precipitation)) %>%
+    .$daily_precip
+
+  return(out)
+}
