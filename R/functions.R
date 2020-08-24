7a9993c4d14973760cc3edd5311100eedd966afe
everydayduffy <james.philip.duffy@gmail.com>
2020-08-24 09:41:03 +0100

    updated list structure for CDS as dictated by ECWMFR

--- R/functions.R
+++ R/functions.R
@@ -210,28 +210,27 @@ build_era5_request <- function(xmin, xmax, ymin, ymax, start_time, end_time,
       dplyr::select(., mon)
 
     sub_request <- list(
-      area = ar,
-      product_type = "reanalysis",
-      format = "netcdf",
-      variable = c("2m_temperature", "2m_dewpoint_temperature", "surface_pressure",
-                   "10m_u_component_of_wind", "10m_v_component_of_wind",
-                   "total_precipitation", "total_cloud_cover",
-                   "mean_surface_net_long_wave_radiation_flux",
-                   "mean_surface_downward_long_wave_radiation_flux",
-                   "total_sky_direct_solar_radiation_at_surface",
-                   "surface_solar_radiation_downwards", "land_sea_mask"),
-      year = as.character(yr),
-      month = as.character(sub_mon$mon),
-      day = c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
+      "dataset_short_name" = "reanalysis-era5-single-levels",
+      "product_type" = "reanalysis",
+      "variable" = c("2m_temperature", "2m_dewpoint_temperature", "surface_pressure",
+                     "10m_u_component_of_wind", "10m_v_component_of_wind",
+                     "total_precipitation", "total_cloud_cover",
+                     "mean_surface_net_long_wave_radiation_flux",
+                     "mean_surface_downward_long_wave_radiation_flux",
+                     "total_sky_direct_solar_radiation_at_surface",
+                     "surface_solar_radiation_downwards", "land_sea_mask"),
+      "year" = as.character(yr),
+      "month" = as.character(sub_mon$mon),
+      "day" = c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
               "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22",
               "23", "24", "25", "26", "27", "28", "29", "30", "31"),
-      time = c("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00",
+      "time" = c("00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00",
                "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00",
                "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00",
                "21:00", "22:00", "23:00"),
-      dataset_short_name = "reanalysis-era5-single-levels",
-      target = paste0(outfile_name,"_",yr,".nc"))
-
+      "area" = ar,
+      "format" = "netcdf",
+      "target" = paste0(outfile_name,"_",yr,".nc"))
     request[[i]] <- sub_request
   }
   return(request)
